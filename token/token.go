ac64f3502d5c077e34d77936e9db143305cb46ce
Jessica Ward <Jessicawardiom@gmail.com>
2020-03-09 22:04:16 +0000

    Added token package

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//Identifiers + literals
+	IDENT = "IDENT" //add, foobar, colour, x, y, z
+	INT   = "INT"   //1234567890
+
+	//Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
