3fa9703c513dbe4566081f078cda4e65e86ddafb
Jessica Ward <Jessicawardiom@gmail.com>
2020-03-09 23:01:31 +0000

    Added Ident lookup in token package and some helper methods in lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -52,6 +52,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -60,4 +67,18 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input(position:l.position)
+}
+
+//This checks if the character being analysed is a valid letter.
+//Not just A-Za-z.. but also allowed special characters
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' || ch == '-' || ch == '@'
 }
