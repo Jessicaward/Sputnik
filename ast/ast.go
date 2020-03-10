0d471475c583698d392dea5ed4b7594cd7956088
Jessica Ward <Jessicawardiom@gmail.com>
2020-03-10 21:01:49 +0000

    Added AST (alongside let statement struct)

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "go/token"
+
+//Everything in AST is a node.
+//There are two types of nodes; statements and expressions.
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+//This has both an identifier and an expression. The expression is for when the identifier is "used"
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) statementNode()       {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
