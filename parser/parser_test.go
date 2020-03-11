0bb6ae0c60b84645afb0f3d3dfe2d5a854d774a5
Jessica Ward <Jessicawardiom@gmail.com>
2020-03-11 17:02:10 +0000

    #3 Add error handling to parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -16,6 +16,7 @@ let foobar = 838838;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -64,4 +65,21 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+//todo: maybe move this to a utilities package?
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
