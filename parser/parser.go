6e02ee6a974a21d514fa4e39c7e36b1980c2dcc4
Jessica Ward <Jessicawardiom@gmail.com>
2020-03-11 16:49:30 +0000

    Replacing current token checks with currentTokenIs method usage.

--- parser/parser.go
+++ parser/parser.go
@@ -32,7 +32,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
 
-	for p.currentToken.Type != token.EOF {
+	for !p.currentTokenIs(token.EOF) {
 		stmt := p.parseStatement()
 
 		if stmt != nil {
